2659e15d845c1293022e61b835f7bcd5f2230f57
Nikolai Korolev <korolevns98@gmail.com>
2025-01-25 01:37:46 +0000

    Rewrite the code to not do constant reconnections

--- localhost-throttle/main.py
+++ localhost-throttle/main.py
@@ -3,51 +3,7 @@ import contextlib
 import threading
 from .parser import create_parser
 from .protocol_type import Protocol
-
-import time
-
-
-class RedirectClient:
-  # TODO: Customize buffer_size
-  def __init__(self, in_socket, out_socket, buffer_size=1000):
-    self.in_socket = in_socket
-    self.out_socket = out_socket
-    self.buffer_size = buffer_size
-    self._stopped = None
-    self._thread_in_to_out = None
-    self._thread_out_to_in = None
-
-  def _start_redirect_blocking_in_to_out(self):
-    in_socket = self.in_socket
-    out_socket = self.out_socket
-    buffer_size = self.buffer_size
-    while not self._stopped.isSet():
-      try:
-        data = in_socket.recv(buffer_size)
-        out_socket.send(data)
-      except OSError:
-        self._stopped.set()
-
-  def _start_redirect_blocking_out_to_in(self):
-    in_socket = self.in_socket
-    out_socket = self.out_socket
-    buffer_size = self.buffer_size
-    while not self._stopped.isSet():
-      try:
-        data = out_socket.recv(buffer_size)
-        in_socket.send(data)
-      except OSError:
-        self._stopped.set()
-
-  def start(self):
-    self._stopped = threading.Event()
-    self._thread_in_to_out = threading.Thread(target=self._start_redirect_blocking_in_to_out)
-    self._thread_in_to_out.start()
-    self._thread_out_to_in = threading.Thread(target=self._start_redirect_blocking_out_to_in)
-    self._thread_out_to_in.start()
-
-  def stop(self):
-    self._stopped.set()
+from .redirect_client import redirect_and_close_on_exception
 
 
 # TODO: Make hostname configurable
@@ -61,14 +17,13 @@ def redirect(protocol, in_port, out_port, hostname="", backlog=100):
     out_socket.listen(backlog)
 
     while True:
-      (clientsocket, _) = out_socket.accept()
-
-      with contextlib.closing(socket.socket(socket.AF_INET, socket_type)) as in_socket:
-        in_socket.connect(("localhost", in_port))
-        redirect_in_to_client = RedirectClient(in_socket, clientsocket)
-        redirect_in_to_client.start()
-        time.sleep(5)
-        clientsocket.close()
+      (client_socket, _) = out_socket.accept()
+      thread = threading.Thread(
+        target=redirect_and_close_on_exception,
+        kwargs={"client_socket": client_socket, "in_port": in_port, "socket_type": socket_type},
+        daemon=True,
+      )
+      thread.start()
 
 
 def localhost_throttle(in_port, out_port, protocols):
---
+++ localhost-throttle/redirect_client.py
@@ -0,0 +1,49 @@
+import contextlib
+import threading
+import socket
+
+
+class RedirectClient:
+  # TODO: Customize buffer_size
+  def __init__(self, in_socket, out_socket, buffer_size=65536):
+    self.in_socket = in_socket
+    self.out_socket = out_socket
+    self.buffer_size = buffer_size
+    self._stopped = None
+    self._thread_in_to_out = None
+    self._thread_out_to_in = None
+
+  def _start_redirect_blocking(self, in_socket, out_socket):
+    buffer_size = self.buffer_size
+    while not self._stopped.isSet():
+      try:
+        data = in_socket.recv(buffer_size)
+        if len(data) == 0:
+          self._stopped.set()
+          break
+        out_socket.send(data)
+      except OSError:
+        self._stopped.set()
+
+  def start(self):
+    self._stopped = threading.Event()
+    self._thread_in_to_out = threading.Thread(
+      target=self._start_redirect_blocking, args=(self.in_socket, self.out_socket), daemon=True
+    )
+    self._thread_in_to_out.start()
+    self._thread_out_to_in = threading.Thread(
+      target=self._start_redirect_blocking, args=(self.out_socket, self.in_socket), daemon=True
+    )
+    self._thread_out_to_in.start()
+
+  def stop(self):
+    self._stopped.set()
+
+
+def redirect_and_close_on_exception(*, client_socket, in_port, socket_type):
+  with contextlib.closing(client_socket) as client_socket:
+    with contextlib.closing(socket.socket(socket.AF_INET, socket_type)) as in_socket:
+      in_socket.connect(("localhost", in_port))
+      redirect_in_to_client = RedirectClient(in_socket, client_socket)
+      redirect_in_to_client.start()
+      redirect_in_to_client._stopped.wait()
