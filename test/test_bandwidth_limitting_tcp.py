89a1177104caae07210dfd9237f0c855b80b630c
Nikolai Korolev <korolevns98@gmail.com>
2025-01-30 23:35:51 +0000

    Update bandwidth limitting tests

--- test/test_bandwidth_limitting_tcp.py
+++ test/test_bandwidth_limitting_tcp.py
@@ -4,7 +4,7 @@ from .util import TCPSingleConnectionTest
 
 
 @pytest.mark.timeout(5)
-def test_bandwidth_slows_down_data_transfer_tcp():
+def test_slows_down_data_transfer_tcp():
   with TCPSingleConnectionTest(bandwidth=5) as (in_socket_out, out_socket, _):
     out_socket.settimeout(0.1)
     data_to_send = b"1"
@@ -13,4 +13,4 @@ def test_bandwidth_slows_down_data_transfer_tcp():
       data_to_receive = out_socket.recv(len(data_to_send))
     out_socket.settimeout(0.5)
     data_to_receive = out_socket.recv(len(data_to_send))
-    assert data_to_send == data_to_receive, "Data received is not equal to data send"
+    assert data_to_send == data_to_receive, f"Data received is not equal to data send. {data_to_send=}, {data_to_receive=}"
--- test/test_bandwidth_limitting_udp.py
+++ test/test_bandwidth_limitting_udp.py
@@ -4,7 +4,7 @@ from .util import UDPSingleConnectionTest
 
 
 @pytest.mark.timeout(3)
-def test_redirects_data_out_to_in():
+def test_slows_down_data_out_to_in():
   with UDPSingleConnectionTest(bandwidth=5) as (in_socket, out_socket, _, out_port):
     data_to_send = b"1"
     out_socket.sendto(data_to_send, ("localhost", out_port))
@@ -13,4 +13,4 @@ def test_redirects_data_out_to_in():
       data_to_receive, _ = in_socket.recvfrom(len(data_to_send))
     in_socket.settimeout(0.5)
     data_to_receive, _ = in_socket.recvfrom(len(data_to_send))
-    assert data_to_send == data_to_receive, "Data received is not equal to data send"
+    assert data_to_send == data_to_receive, f"Data received is not equal to data send. {data_to_send=}, {data_to_receive=}"
