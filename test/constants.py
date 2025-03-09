0f7755af47a306bb8a6cc560a3ccad3082fab8c3
Nikolai Korolev <korolevns98@gmail.com>
2025-03-09 11:28:41 +0000

    Give more time to finish process

--- test/constants.py
+++ test/constants.py
@@ -3,4 +3,4 @@ import os
 DEFAULT_CWD = os.path.dirname(os.path.dirname(__file__))
 MODULE_NAME = "localhost_throttle"
 DELAY_TO_START_UP = 0.5
-TIME_FOR_PROCESS_TO_FINISH = 0.5
+TIME_FOR_PROCESS_TO_FINISH = 0.8
